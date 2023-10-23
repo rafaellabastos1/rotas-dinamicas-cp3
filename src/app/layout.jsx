8fc3dd46ddf934f9a0f9b88ec5d59998712e6053
douglas <douglas.arau@hotmail.com>
2023-10-23 18:09:22 -0300

    sou um bosta 123

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Cabecalho from './components/Cabecalho/page'
+import Cabecalho from '../components/Cabecalho/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
