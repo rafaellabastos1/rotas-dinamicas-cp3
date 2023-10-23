37d78f14ad46ec512fc47777d46192d575279c3b
rafaellabastos1 <rm552425@fiap.com.br>
2023-10-23 16:50:05 -0300

    coloquei os links no cabeçalho e passei para a home

---
+++ src/app/components/Cabecalho/page.jsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Cabecalho() {
+    return (
+        <nav>
+            <Link href="/produtos/item/vestuario/slug">Produtos</Link>
+        </nav>
+    )
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Cabecalho from './components/Cabecalho/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>
+        <Cabecalho/>
         {children}
       </body>
     </html>
